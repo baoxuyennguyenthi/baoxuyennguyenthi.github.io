707d13ddd4aeebbfb00549f2617808008492f237
Xuyen Nguyen <baoxuyennguyenthi@gmail.com>
2021-06-02 20:37:39 +0700

    update 2.6

--- js/app.js
+++ js/app.js
@@ -22,13 +22,6 @@ $slick.slick({
                 initialSlide: 1
             }
         },
-        // {
-        //     breakpoint: 480,
-        //     settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //     }
-        // }
     ]
 
 });
@@ -38,3 +31,91 @@ $('.prev-btn').click(function () {
 $('.next-btn').click(function () {
     $slick.slick('slickNext');
 })
+
+
+
+$(document).ready(function () {
+    var submitIcon = $('.searchbar-icon');
+    var inputBox = $('.searchbar-input');
+    var searchbar = $('.searchbar');
+    var isOpen = false;
+    submitIcon.click(function () {
+        if (isOpen == false) {
+            searchbar.addClass('searchbar-open');
+            inputBox.focus();
+            isOpen = true;
+        } else {
+            searchbar.removeClass('searchbar-open');
+            inputBox.focusout();
+            isOpen = false;
+        }
+    });
+    submitIcon.mouseup(function () {
+        return false;
+    });
+    searchbar.mouseup(function () {
+        return false;
+    });
+    $(document).mouseup(function () {
+        if (isOpen == true) {
+            $('.searchbar-icon').css('display', 'block');
+            submitIcon.click();
+        }
+    });
+});
+function buttonUp() {
+    var inputVal = $('.searchbar-input').val();
+    inputVal = $.trim(inputVal).length;
+    if (inputVal !== 0) {
+        $('.searchbar-icon').css('display', 'none');
+    } else {
+        $('.searchbar-input').val('');
+        $('.searchbar-icon').css('display', 'block');
+    }
+}
+
+//menu scroll
+
+window.onscroll = function () { scrollFunction() };
+menu = document.getElementById("menu-main");
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        menu.classList.add("fixed-top")
+    } else {
+        menu.classList.remove("fixed-top")
+    }
+}
+
+//
+
+var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
+
+nav.find('a').on('click', function () {
+    var $el = $(this)
+        , id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - 100
+    }, 150);
+
+    return false;
+});
